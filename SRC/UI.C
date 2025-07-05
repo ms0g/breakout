8b18c61efd9776cf62215692f0dbca0af81b793f
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-07-05 04:02:14 +0300

    refactor(UI): replace inline position struct with ObjectProperty for improved clarity and consistency

--- SRC/UI.C
+++ SRC/UI.C
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "FONTS.H"
 #include "RENDERER.H"
+#include "OBJPRTY.H"
 
 #define SCORE_X 12
 #define SCORE_Y 10
@@ -13,17 +14,12 @@
 #define ADJ 6
 
 typedef struct {
-    struct {
-        int x;
-        int y;
-    } position;
+    ObjectProperty property;
     
     union {
         int num;
         const char* str;
     } val;
-    
-    char color;
 } UIElement;
 
 static UIElement scoreText;
@@ -37,31 +33,31 @@ static void drawText(const char* s, int x, int y, char color);
 static void drawNumber(int num, int x, int y, char color);
 
 void uiInit(void) {
-    scoreText.position.x = SCORE_X;
-    scoreText.position.y = SCORE_Y;
+    scoreText.property.position.x = SCORE_X;
+    scoreText.property.position.y = SCORE_Y;
+    scoreText.property.color = 0xF;
     scoreText.val.str = "SCORE:";
-    scoreText.color = 0xF;
-
-    scoreNum.position.x = SCORE_X + SCORE_ADJ;
-    scoreNum.position.y = SCORE_Y;
+    
+    scoreNum.property.position.x = SCORE_X + SCORE_ADJ;
+    scoreNum.property.position.y = SCORE_Y;
+    scoreNum.property.color = 0xF;
     scoreNum.val.num = 0;
-    scoreNum.color = 0xF;
 
-    lifeText.position.x = LIFE_X;
-    lifeText.position.y = LIFE_Y;
+    lifeText.property.position.x = LIFE_X;
+    lifeText.property.position.y = LIFE_Y;
+    lifeText.property.color = 0xF;
     lifeText.val.str = "LIFE:";
-    lifeText.color = 0xF;
-
-    lifeNum.position.x = LIFE_X + LIFE_ADJ;
-    lifeNum.position.y = LIFE_Y;
+   
+    lifeNum.property.position.x = LIFE_X + LIFE_ADJ;
+    lifeNum.property.position.y = LIFE_Y;
+    lifeNum.property.color = 0xF;
     lifeNum.val.num = 0;
-    lifeNum.color = 0xF;
-
-    gameOverText.position.x = 130;
-    gameOverText.position.y = 130;
+   
+    gameOverText.property.position.x = 130;
+    gameOverText.property.position.y = 130;
+    gameOverText.property.color = 0xF;
     gameOverText.val.str = "GAME OVER";
-    gameOverText.color = 0xF;
-
+   
     isGameOver = 0;
 }
 
@@ -71,14 +67,14 @@ void uiUpdate(int score, int life) {
 }
 
 void uiDraw(void) {
-    drawText(scoreText.val.str, scoreText.position.x, scoreText.position.y, scoreText.color);
-    drawNumber(scoreNum.val.num, scoreNum.position.x, scoreNum.position.y, scoreNum.color);
+    drawText(scoreText.val.str, scoreText.property.position.x, scoreText.property.position.y, scoreText.property.color);
+    drawNumber(scoreNum.val.num, scoreNum.property.position.x, scoreNum.property.position.y, scoreNum.property.color);
     
-    drawText(lifeText.val.str, lifeText.position.x, lifeText.position.y, lifeText.color);
-    drawNumber(lifeNum.val.num, lifeNum.position.x, lifeNum.position.y, lifeNum.color);
+    drawText(lifeText.val.str, lifeText.property.position.x, lifeText.property.position.y, lifeText.property.color);
+    drawNumber(lifeNum.val.num, lifeNum.property.position.x, lifeNum.property.position.y, lifeNum.property.color);
 
     if (isGameOver) {
-        drawText(gameOverText.val.str, gameOverText.position.x, gameOverText.position.y, gameOverText.color);
+        drawText(gameOverText.val.str, gameOverText.property.position.x, gameOverText.property.position.y, gameOverText.property.color);
     }
 }
 
